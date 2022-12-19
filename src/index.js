ea76898531e1498cc7c83b45241bcdbc9a78cff9
Harimad <leesh68481@gmail.com>
2022-12-19 17:23:19 +0900

    241. 핵심 리덕스 개념 탐색하기

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App'
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
-  // <React.StrictMode>
-  <App />
-  // </React.StrictMode>
-)
+// const root = ReactDOM.createRoot(document.getElementById('root'))
+// root.render(
+//   // <React.StrictMode>
+//   <App />
+//   // </React.StrictMode>
+// )
+
+const redux = require('redux')
+
+const counterReducer = (state = { counter: 0 }, action) => {
+  return {
+    counter: state.counter + 1,
+  }
+}
+
+const store = redux.createStore(counterReducer)
+
+console.log(store.getState())
+
+const coutnerSubscriber = () => {
+  const latestState = store.getState()
+  console.log(latestState)
+}
+
+store.subscribe(coutnerSubscriber)
+
+store.dispatch({ type: 'increment' })
