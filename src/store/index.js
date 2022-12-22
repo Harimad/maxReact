0d095f021ea64a8cb379f0396059b3d3b86d5bd6
Harimad <leesh68481@gmail.com>
2022-12-22 22:36:22 +0900

    264. 복습/연습: 2/2부

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+
 import uiSlice from "./ui-slice";
+import cartSlice from "./card-slice";
 
 const store = configureStore({
-  reducer: { ui: uiSlice.reducer },
+  reducer: { ui: uiSlice.reducer, cart: cartSlice.reducer },
 });
 
 export default store;
