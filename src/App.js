8a0f5ba29006c283925f68aa16524586461ef053
Harimad <leesh68481@gmail.com>
2022-12-22 17:52:27 +0900

    redux-toolkit-thunk for Aync

--- src/App.js
+++ src/App.js
@@ -1,7 +1,49 @@
-import './App.css'
+import React from "react";
+import { createStore } from "redux";
+import { Provider, useSelector, useDispatch } from "react-redux";
+import store from "./store";
+import { up } from "./counterSlice";
+import { asyncUpFetch } from "./counterSlice";
 
-function App() {
-  return <div className="App">App</div>
+function Counter() {
+  const dispatch = useDispatch();
+  const count = useSelector(state => {
+    return state.counter.value;
+  });
+  const status = useSelector(state => {
+    return state.counter.status;
+  });
+  return (
+    <div>
+      <button
+        onClick={() => {
+          dispatch(up(2));
+        }}
+      >
+        +
+      </button>
+
+      <button
+        onClick={() => {
+          dispatch(asyncUpFetch());
+        }}
+      >
+        + async fetch
+      </button>
+      <br />
+
+      <div>
+        {count} | {status}
+      </div>
+    </div>
+  );
 }
-
-export default App
+export default function App() {
+  return (
+    <Provider store={store}>
+      <div>
+        <Counter></Counter>
+      </div>
+    </Provider>
+  );
+}
